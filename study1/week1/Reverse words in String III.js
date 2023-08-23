113630da05a96246472a35eb60381fe67e725830
Seung-Jae Yoon <casysj@hotmail.com>
2023-08-24 00:53:58 +0200

    a

--- study1/week1/Reverse words in String III.js
+++ study1/week1/Reverse words in String III.js
@@ -35,7 +35,7 @@ reverseWords('hello world man! zahooyy');
 //  O(n^2)
 /**
  *  for문이 있고 그 안에 돌아가는 reverseString이 재귀형태로 작동해서
- *  이것도 마찬가지로 O(n^2) 이다
+ *  이것도 마찬가지로 O(n^2) 이다.
  */
 var reverseWords = function(s) {
     let returnVal ='';
