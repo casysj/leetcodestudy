d66c8674febd4dcf231e71ebd6258c03c2c1f69a
Seung-Jae Yoon <casysj@hotmail.com>
2023-09-16 01:19:15 +0200

    week4-3

--- study1/week4/Rotate Image.js
+++ study1/week4/Rotate Image.js
@@ -2,6 +2,11 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
+/**
+*	시간복잡도 : O(n) 배열의 모든 요소들을 방문해서 처리한다
+*	공간복잡도 : O(1) 배열의 크기와 관계없이 고정된 변수만 사용함
+*/
+
 var rotate = function (matrix) {
     let storeTemp1;
     let storeTemp2;
